8d1b0af04c0179695e32b5f94914d17d69d02eca
Eswar <eswar.chandana@apoorva.com>
2025-11-17 21:32:11 +0530

    colors

--- redis.sh
+++ redis.sh
@@ -43,7 +43,7 @@ VALIDATE $? "Enabling redis module"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "install Redis "
 
-sed -i 's/127.0.0.1/0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Edited redis.conf to accept remote connections "
 
 systemctl enable redis &>>$LOG_FILE
