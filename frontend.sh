223956ef7c90867ce12e58274b114d0c29b8845f
Eswar <eswar.chandana@apoorva.com>
2025-11-18 13:14:15 +0530

    colors

--- frontend.sh
+++ frontend.sh
@@ -1,36 +1,7 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD  #starting ekkada undho adhi variable laga tesukuntundhi
-
-mkdir -p $LOGS_FOLDER
-echo "script started executing at: $(date)" | tee -a $LOG_FILE
-
-# check the user has root privilages or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root Access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
-
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-    fi
-}
+source ./common.sh
+check_root
 
 dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "Disabling default nginx"
@@ -64,3 +35,5 @@ VALIDATE $? "Copying nginx conf"
 
 systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "restarting nginx conf"
+
+print_time
