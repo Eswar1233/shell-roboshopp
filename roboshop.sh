7b25bf5c005006d2665fb981aeb1fec5fd355c6c
Eswar <eswar.chandana@apoorva.com>
2025-11-19 06:51:00 +0530

    hello world

--- roboshop.sh
+++ roboshop.sh
@@ -13,6 +13,24 @@ DOMAIN_NAME="squareladdu.in" # present in route 53
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-06d806d4818b5c861 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+
+     # -------------------------
+    # WAIT UNTIL IP IS ASSIGNED
+    # -------------------------
+    IP=""
+    while [ -z "$IP" ] || [ "$IP" == "None" ]
+    do
+        sleep 3
+        if [ "$instance" != "frontend" ]; then
+            IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+                --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        else
+            IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+                --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        fi
+    done
+    # -------------------------
+
     if [ $instance != "frontend" ]
     then 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
